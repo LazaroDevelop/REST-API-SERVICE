971b10a00cf4cd11e3b5cba944dc83c747d66a04
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-04-29 14:20:18 -0400

    add product data transfer object

---
+++ src/main/java/net/space/developer/restapiservice/model/ProductDTO.java
@@ -0,0 +1,20 @@
+package net.space.developer.restapiservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Product document class to describe product properties
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-04-29
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+}
