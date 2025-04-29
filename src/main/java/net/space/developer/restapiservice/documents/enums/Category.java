1ead8abb15c54d8c17805eab0c9cb2db08a011a6
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-04-29 14:15:57 -0400

    add category enum for products

---
+++ src/main/java/net/space/developer/restapiservice/documents/enums/Category.java
@@ -0,0 +1,12 @@
+package net.space.developer.restapiservice.documents.enums;
+
+/**
+ * Category enum class to list products categories
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-04-29
+ */
+
+public enum Category {
+    FOOD, CLOTHES, HOME_APPLIANCES, SHOES
+}
