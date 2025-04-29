5abb8874009c9a06f5412f0281784327c225c093
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-04-29 15:11:25 -0400

    update product repository

--- src/main/java/net/space/developer/restapiservice/repository/ProductRepository.java
+++ src/main/java/net/space/developer/restapiservice/repository/ProductRepository.java
@@ -1,9 +1,20 @@
 package net.space.developer.restapiservice.repository;
 
 import net.space.developer.restapiservice.documents.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Product repository class
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-04-29
+ */
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
+    Page<Product> findByCategory(String category,Pageable pageable);
+    Page<Product> findByNameContainingIgnoreCase(String name,Pageable pageable);
 }
