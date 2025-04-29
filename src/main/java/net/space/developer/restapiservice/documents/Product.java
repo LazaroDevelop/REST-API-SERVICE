ba1dcd09be36a2a377116a166ea96d4ea52ede06
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-04-29 14:15:44 -0400

    add product document

---
+++ src/main/java/net/space/developer/restapiservice/documents/Product.java
@@ -0,0 +1,59 @@
+package net.space.developer.restapiservice.documents;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.space.developer.restapiservice.documents.enums.Category;
+import net.space.developer.restapiservice.documents.enums.State;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+/**
+ * Product document class to describe product properties
+ *
+ * @author Lazaro Noel Guerra Medina
+ * @since 2025-04-29
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+
+    @Field
+    @NotNull
+    private String name;
+
+    @Field
+    private String description;
+
+    @Field
+    @NotNull
+    private Category category;
+
+    @Field
+    @NotNull
+    private Double price;
+
+    @Field
+    private List<String> images;
+
+    @Field
+    private String brand;
+
+    @Field
+    @NotNull
+    private String batch;
+
+    @Field
+    @NotNull
+    private State state;
+}
