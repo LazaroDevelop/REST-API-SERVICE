7b46c19093f6e298e354f8081221a608b1ea47d4
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-02 21:05:40 -0400

    enable caching

--- src/main/java/net/space/developer/restapiservice/RestApiServiceApplication.java
+++ src/main/java/net/space/developer/restapiservice/RestApiServiceApplication.java
@@ -2,7 +2,9 @@ package net.space.developer.restapiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class RestApiServiceApplication {
 
