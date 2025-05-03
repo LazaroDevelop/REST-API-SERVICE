b45c4091bc43ae7cb203734df81a6e8bb8fe9311
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-02 20:56:42 -0400

    add application constants

---
+++ src/main/java/net/space/developer/restapiservice/common/utility/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package net.space.developer.restapiservice.common.utility;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApplicationConstants {
+
+    public final String PRODUCT_NOT_FOUND_MESSAGE =  "Product not found exception";
+
+}
