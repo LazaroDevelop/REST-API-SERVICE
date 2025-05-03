ec569195de4c7dbbd101898bc5e2efd8fb10411f
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-02 22:14:22 -0400

    add product cache constants

--- src/main/java/net/space/developer/restapiservice/common/utility/ApplicationConstants.java
+++ src/main/java/net/space/developer/restapiservice/common/utility/ApplicationConstants.java
@@ -5,6 +5,11 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ApplicationConstants {
 
+    // Exception constants
     public final String PRODUCT_NOT_FOUND_MESSAGE =  "Product not found exception";
+
+    //Cache constants
+    public final String PRODUCT_CACHE = "product";
+    public final String PRODUCTS_CACHE = "products";
 
 }
