ee1e4540451fc4d874c31c94b2fb446cc0f12132
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-02 21:10:13 -0400

    finish cache configuration

--- src/main/java/net/space/developer/restapiservice/config/CacheConfig.java
+++ src/main/java/net/space/developer/restapiservice/config/CacheConfig.java
@@ -1,2 +1,28 @@
-package net.space.developer.restapiservice.config;public class CacheConfig {
+package net.space.developer.restapiservice.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(){
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("localCache");
+        cacheManager.setCaffeine(caffeineBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(Duration.ofMinutes(10));
+    }
+
 }
