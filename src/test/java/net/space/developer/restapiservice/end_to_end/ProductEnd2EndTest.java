98530f3e8cb086fade3b4551d3dd42556b045d6c
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-03 14:18:35 -0400

    add end-2-end empty class

---
+++ src/test/java/net/space/developer/restapiservice/end_to_end/ProductEnd2EndTest.java
@@ -0,0 +1,10 @@
+package net.space.developer.restapiservice.end_to_end;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@WebMvcTest
+@SpringBootTest
+public class ProductEnd2EndTest {
+}
